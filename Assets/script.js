3d22db59f5ace72edcde3d1471d8c56d1401df70
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-03-15 21:39:55 -0600

    assignment of fixed variables

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Assignment of fixed variables
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var upperLower = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+var numbers = '1234567890';
+var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
