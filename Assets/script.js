1dc35b9c6c2eedf9237db218a2c7a23999ee0af4
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-03-15 21:45:11 -0600

    correct identation

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,7 @@ var upperLower = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
 var numbers = '1234567890';
 var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+
 // Write password to the #password input
 function writePassword() {
     var CaseResult = '';
@@ -24,7 +25,7 @@ function writePassword() {
       }
     var lengthInt = parseInt(passwordLength, 10);   // Password length to number
   
-  //Evaluate prompt's results in order to generate the same amount of letters, numbers and special characters
+    //Evaluate prompt's results in order to generate the same amount of letters, numbers and special characters
     if(passwordNumeric == true && passwordCharacters == true){
       var length = Math.ceil(lengthInt / 3);
     }else if (passwordNumeric == true || passwordCharacters == true){
@@ -34,59 +35,58 @@ function writePassword() {
     }
 
     // This function generates a string of random characters and will be applied to every type of character
-  function generateString(length, variable) {
-    let Result = '';
-    const charactersLength = variable.length;1
-    for ( let i = 0; i < length; i++ ) {
-        Result += variable.charAt(Math.floor(Math.random() * charactersLength));
+    function generateString(length, variable) {
+        let Result = '';
+        const charactersLength = variable.length;1
+        for ( let i = 0; i < length; i++ ) {
+            Result += variable.charAt(Math.floor(Math.random() * charactersLength));
+        }
+    
+        return Result;
     }
-  
-    return Result;
-  }
 
-  // This switch section decides between prodividg random uppercase, lowercase or both
-  switch(passwordCase){
-    case '1':
-      CaseResult = generateString(length, lowercase);
-      break;
-    case '2':
-      CaseResult = generateString(length, uppercase);
-      break;
-    case '3':
-      CaseResult = generateString(length, upperLower);
-      break;
-    default:
-      CaseResult = '';
-  }
+    // This switch section decides between prodividg random uppercase, lowercase or both
+    switch(passwordCase){
+        case '1':
+        CaseResult = generateString(length, lowercase);
+        break;
+        case '2':
+        CaseResult = generateString(length, uppercase);
+        break;
+        case '3':
+        CaseResult = generateString(length, upperLower);
+        break;
+        default:
+        CaseResult = '';
+    }
 
-  // Generates a string of random numbers
-  if(passwordNumeric){
-    numbersResult = generateString(length, numbers);
-  }else{
-    numbersResult = '';
-  }
+    // Generates a string of random numbers
+    if(passwordNumeric){
+        numbersResult = generateString(length, numbers);
+    }else{
+        numbersResult = '';
+    }
 
-// Generates a string of random special characters
-  if(passwordCharacters){
-   charactersResult = generateString(length, specialCharacters);
-  }else{
-    charactersResult = '';
-  }
+    // Generates a string of random special characters
+    if(passwordCharacters){
+    charactersResult = generateString(length, specialCharacters);
+    }else{
+        charactersResult = '';
+    }
 
-//Concatenates every type of character string
-result = CaseResult + numbersResult + charactersResult;
+    //Concatenates every type of character string
+    result = CaseResult + numbersResult + charactersResult;
 
-// Given that the previous variable is ordered by character type, it is neccesary to split - shuffle - join
-  var shuffledResult = result.split('').sort(function(){return 0.5-Math.random()}).join('');
+    // Given that the previous variable is ordered by character type, it is neccesary to split - shuffle - join
+    var shuffledResult = result.split('').sort(function(){return 0.5-Math.random()}).join('');
 
-// Making sure the final password has the required lenght
-  var password = shuffledResult.substring(0, lengthInt);
+    // Making sure the final password has the required lenght
+    var password = shuffledResult.substring(0, lengthInt);
 
-  var passwordText = document.querySelector("#password");
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 }  
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
