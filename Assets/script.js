76ecbcdd6943976a26853833a416f29e786a396c
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-03-16 19:12:09 -0600

    length correction

--- Assets/script.js
+++ Assets/script.js
@@ -7,20 +7,20 @@ var upperLower = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
 var numbers = '1234567890';
 var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-
 // Write password to the #password input
 function writePassword() {
     var CaseResult = '';
     var numbersResult = '';
     var charactersResult = '';
-    var passwordCase = prompt("¿Lowercase (1), uppercase (2) or both (3)?");
+    alert("Please specify the password criteria answering the following questions")
+    var passwordCase = prompt("¿Do you want it to use lowercase (1), uppercase (2) or both (3)?");
       if(passwordCase != "1" && passwordCase != "2" && passwordCase != "3"){
         passwordCase =  prompt("Please introduce only a number between 1 and 3... ¿Lowercase (1), uppercase (2) or both (3)?")
       }
-    var passwordNumeric = confirm("include numbers?");
-    var passwordCharacters = confirm("include special characters?");
-    var passwordLength = prompt("Specify the password length (8-128 characters");
-      if(passwordLength < "8" && passwordLength > "128"){
+    var passwordNumeric = confirm("¿Do you want it to include numbers?");
+    var passwordCharacters = confirm("¿Do you want it to include special characters?");
+    var passwordLength = prompt("¿What length do you want the password to have (8-128 characters)?");
+      if(parseInt(passwordLength, 10) < 8 || parseInt(passwordLength, 10) > 128){
         passwordLength = prompt("Please specify the password lenght with a number between 8 and 128");
       }
     var lengthInt = parseInt(passwordLength, 10);   // Password length to number
